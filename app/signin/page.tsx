be0cc3d190e6e6c9828a3d338fdb446b55ccc089
contatothiagoisn-lang <contatothiagoisn@gmail.com>
2025-12-02 20:06:17 -0300

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,9 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-'use client'
 export const dynamic = 'force-dynamic'
-
 
 export default function SignIn() {
   const supabase = createClient(
@@ -17,12 +15,10 @@ export default function SignIn() {
       <div className="text-center">
         <h1 className="text-7xl font-black mb-12 text-white">PixNinja</h1>
         <button
-          onClick={() => 
-            supabase.auth.signInWithOAuth({
-              provider: 'google',
-              options: { redirectTo: location.origin + '/dashboard' }
-            })
-          }
+          onClick={()=> supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: { redirectTo: location.origin + '/dashboard' }
+          })}
           className="bg-emerald-500 hover:bg-emerald-400 text-black font-bold py-8 px-24 rounded-3xl text-3xl shadow-2xl transition-all transform hover:scale-105"
         >
           Entrar com Google
